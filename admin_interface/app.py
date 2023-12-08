622eadbe40829bbb25f2af8e8f670b9ccb5fe7f9
Jongyeol Yang <yangjy0113@gmail.com>
2023-12-08 11:26:03 +0000

    improve functions

--- admin_interface/app.py
+++ admin_interface/app.py
@@ -4,29 +4,109 @@ import requests
 
 API_BASE_URL = "http://localhost:5000/api"
 
+# admin_interface/app.py 내의 add_quiz 함수
 def add_quiz():
     st.subheader("새 퀴즈 추가")
     question_text = st.text_area("문제 내용")
-    correct_answer = st.selectbox("정답", ["A", "B", "C", "D"])
-    if st.button("퀴즈 추가"):
-        # API 호출하여 퀴즈 추가
-        pass
+    
+    answer_type = st.radio("답변 유형 선택", ('A-D', 'O-X'))
 
+    if answer_type == 'A-D':
+        correct_answer = st.selectbox("정답", ["A", "B", "C", "D"])
+    else:
+        correct_answer = st.selectbox("정답", ["O", "X"])
+
+    if st.button("퀴즈 추가"):
+        response = requests.post(f"{API_BASE_URL}/quiz", json={
+            "question_text": question_text,
+            "correct_answer": correct_answer
+        })
+        if response.status_code == 200:
+            st.success("퀴즈가 추가되었습니다.")
+        else:
+            st.error("퀴즈 추가에 실패했습니다.")
+
+# admin_interface/app.py 내의 manage_quiz 함수
 def manage_quiz():
     st.subheader("퀴즈 관리")
-    # API 호출하여 퀴즈 목록 조회
-    # 퀴즈 수정 및 삭제 기능 구현
-    pass
+    quizzes = get_quizzes()
 
+    for i, quiz in enumerate(quizzes, start=1):
+        with st.expander(f"ID: {quiz['id']} - 문제: {quiz['question_text']}"):
+            updated_text = st.text_area(f"문제 수정 {i}", value=quiz['question_text'])
+            updated_answer = st.selectbox(f"정답 수정 {i}", ["A", "B", "C", "D"], index=["A", "B", "C", "D"].index(quiz['correct_answer']), key=f"answer_{i}")
+            if st.button(f"ID {quiz['id']} 수정", key=f"update_{i}"):
+                response = requests.put(f"{API_BASE_URL}/quiz/{quiz['id']}", json={
+                    "question_text": updated_text,
+                    "correct_answer": updated_answer
+                })
+                if response.status_code == 200:
+                    st.success("퀴즈가 수정되었습니다.")
+                else:
+                    st.error("퀴즈 수정에 실패했습니다.")
+            if st.button(f"ID {quiz['id']} 삭제", key=f"delete_{i}"):
+                response = requests.delete(f"{API_BASE_URL}/quiz/{quiz['id']}")
+                if response.status_code == 200:
+                    st.success("퀴즈가 삭제되었습니다.")
+                else:
+                    st.error("퀴즈 삭제에 실패했습니다.")
+
+# admin_interface/app.py 내의 view_responses 함수
 def view_responses():
     st.subheader("퀴즈 응답 조회")
-    # API 호출하여 퀴즈 응답 조회
-    pass
+    quiz_id = st.selectbox("퀴즈 선택", options=get_quiz_ids())
+    if quiz_id:
+        if st.button("결과 조회"):
+            # 선택한 퀴즈에 대한 응답 결과 요청
+            responses = requests.get(f"{API_BASE_URL}/quiz/responses/{quiz_id}").json()
+            correct_responses = [resp for resp in responses if resp['is_correct']]
+            correct_count = len(correct_responses)
+            total_count = len(responses)
 
+            # 결과 표시
+            st.write(f"정답률: {correct_count / total_count * 100:.2f}% ({correct_count}/{total_count})")
+            for response in correct_responses:
+                st.write(f"Device {response['device_id']} - 정답")
+
+            # 정답 디바이스에게 명령어 보내기
+            if st.button("정답 디바이스에게 명령어 보내기"):
+                for device_id in [resp['device_id'] for resp in correct_responses]:
+                    requests.post(f"{API_BASE_URL}/devices/command/{device_id}", json={"command": "some_command"})
+
+# admin_interface/app.py 내의 get_quiz_ids 함수
+def get_quiz_ids():
+    response = requests.get(f"{API_BASE_URL}/quiz")
+    if response.status_code == 200:
+        quizzes = response.json()
+        return [quiz['id'] for quiz in quizzes]
+    else:
+        st.error("퀴즈 정보를 가져오는 데 실패했습니다.")
+        return []
+
+# admin_interface/app.py 내의 get_quizzes 함수
+def get_quizzes():
+    response = requests.get(f"{API_BASE_URL}/quiz")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        st.error("퀴즈 정보를 가져오는 데 실패했습니다.")
+        return []
+
+
+# admin_interface/app.py 내의 manage_devices 함수
 def manage_devices():
     st.subheader("장비 관리")
-    # API 호출하여 장비 상태 조회 및 제어
-    pass
+    devices = requests.get(f"{API_BASE_URL}/devices").json()
+
+    if devices:
+        for device in devices:
+            st.write(f"Device ID: {device['id']}, 상태: {device['status']}")
+            if st.button(f"Device {device['id']} 재설정"):
+                # 장비 재설정을 위한 API 호출
+                requests.post(f"{API_BASE_URL}/devices/reset/{device['id']}")
+    else:
+        st.write("등록된 장비가 없습니다.")
+
 
 def main():
     st.title("퀴즈 시스템 관리자 대시보드")
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -8,16 +8,107 @@ def configure_routes(app):
     def index():
         return 'Welcome to the Quiz System!'
 
+    @app.route('/api/quiz', methods=['POST'])
+    def add_quiz():
+        data = request.json
+        db = get_db()
+        cursor = db.cursor()
+        try:
+            cursor.execute("INSERT INTO QuizQuestions (QuestionText, CorrectAnswer) VALUES (%s, %s)",
+                        (data['question_text'], data['correct_answer']))
+            db.commit()
+            return {"message": "Quiz added successfully"}, 200
+        except Exception as e:
+            return {"error": str(e)}, 500
+
     @app.route('/api/quiz', methods=['GET', 'POST'])
     def manage_quiz():
+        db = get_db()
+        cursor = db.cursor()
+
         if request.method == 'POST':
             # 새로운 퀴즈 데이터를 저장하는 로직
-            # 예시: request.json을 사용하여 JSON 데이터 처리
-            pass
+            data = request.json
+            try:
+                cursor.execute("INSERT INTO QuizQuestions (QuestionText, CorrectAnswer) VALUES (%s, %s)",
+                            (data['question_text'], data['correct_answer']))
+                db.commit()
+                return {"message": "Quiz added successfully"}, 200
+            except Exception as e:
+                return {"error": str(e)}, 500
         else:
             # 퀴즈 데이터를 반환하는 로직
-            # 예시: 데이터베이스에서 퀴즈 정보를 조회하여 반환
-            pass
-        return jsonify({'message': 'Handled quiz request'})
+            cursor.execute("SELECT * FROM QuizQuestions")
+            quizzes = cursor.fetchall()
+            return jsonify(quizzes)
 
-    # 추가적인 라우트 및 API 엔드포인트를 여기에 구성할 수 있습니다.
+    # flask_app/routes.py 내의 추가 엔드포인트
+    @app.route('/api/quiz', methods=['GET'])
+    def get_quizzes():
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM QuizQuestions")
+        quizzes = cursor.fetchall()
+        return jsonify(quizzes)
+
+    @app.route('/api/quiz/<int:quiz_id>', methods=['PUT'])
+    def update_quiz(quiz_id):
+        data = request.json
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("UPDATE QuizQuestions SET QuestionText = %s, CorrectAnswer = %s WHERE id = %s",
+                    (data['question_text'], data['correct_answer'], quiz_id))
+        db.commit()
+        return {"message": "Quiz updated successfully"}, 200
+
+    @app.route('/api/quiz/<int:quiz_id>', methods=['DELETE'])
+    def delete_quiz(quiz_id):
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("DELETE FROM QuizQuestions WHERE id = %s", (quiz_id,))
+        db.commit()
+        return {"message": "Quiz deleted successfully"}, 200
+
+    # flask_app/routes.py
+    @app.route('/api/devices', methods=['GET'])
+    def get_devices():
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM Devices")
+        devices = cursor.fetchall()
+        return jsonify(devices)
+
+    # flask_app/routes.py
+    @app.route('/api/devices/reset/<int:device_id>', methods=['POST'])
+    def reset_device(device_id):
+        db = get_db()
+        cursor = db.cursor()
+        try:
+            # 장비 상태를 재설정하는 로직 (예: 상태를 '초기화'로 설정)
+            cursor.execute("UPDATE Devices SET Status = '초기화' WHERE id = %s", (device_id,))
+            db.commit()
+            return {"message": f"Device {device_id} reset successfully"}, 200
+        except Exception as e:
+            return {"error": str(e)}, 500
+
+    # flask_app/routes.py
+    @app.route('/api/quiz/responses/<int:quiz_id>', methods=['GET'])
+    def get_quiz_responses(quiz_id):
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("""
+            SELECT qr.DeviceID as device_id, (qq.CorrectAnswer = qr.SelectedAnswer) as is_correct
+            FROM QuizResponses qr
+            JOIN QuizQuestions qq ON qr.QuestionID = qq.id
+            WHERE qr.QuestionID = %s
+        """, (quiz_id,))
+        responses = cursor.fetchall()
+        return jsonify(responses)
+
+    @app.route('/api/devices/command/<int:device_id>', methods=['POST'])
+    def send_command_to_device(device_id):
+        data = request.json
+        command = data.get("command")
+        # 장비에 명령어를 보내는 로직
+        # 예: MQTT 또는 다른 프로토콜을 사용하여 장비에 명령어 전송
+        return {"message": f"Command sent to device {device_id}"}, 200
